e5bb1d6be1883cdd5a5416770e10c5071f3b1acf
tyl-r <99793303+tyl-r@users.noreply.github.com>
2022-10-08 19:56:34 -0700

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -1,2 +1,11 @@
 @ECHO OFF
 
+:: Batch script to open multiple applications on startup
+
+:: Notepad++
+ECHO Starting Notepad++...
+START /D "C:\Program Files\Notepad++\" notepad++.exe
+
+:: Firefox
+ECHO Starting Firefox...
+START /D "C:\Program Files\Mozilla Firefox\" firefox.exe
